1987f2888a669334acf532369420b3a0716b1162
devethan <devohno.ko@gmail.com>
2020-06-08 15:45:09 +0900

    Init

--- stories/dooboo-ui/Accordion.stories.tsx
+++ stories/dooboo-ui/Accordion.stories.tsx
@@ -1,6 +1,5 @@
 import { IC_ARR_DOWN, IC_ARR_UP } from '../Icon';
 import React, { ReactElement } from 'react';
-import { ScrollView, Text, View } from 'react-native';
 
 import Accordion from '../../main/Accordion';
 import { ContainerDeco } from '../../storybook/decorators';
@@ -15,70 +14,10 @@ const Container = styled.SafeAreaView`
   justify-content: center;
 `;
 
-const StyledImage = styled.Image`
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  right: 16px;
-`;
-
 const Default = (): React.ReactElement => {
-  const contents = [
-    {
-      title: 'Title 1',
-      body: 'Hi. I love this component. What do you think?',
-    },
-    {
-      title: 'See this one too',
-      body: 'Yes. You can have more items.',
-    },
-    {
-      title: 'Thrid thing',
-      body: `What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?`,
-    },
-  ];
-
   return (
     <Container>
-      <ScrollView style={{ alignSelf: 'stretch', paddingHorizontal: 20 }}>
-        {contents.map((param, i) => {
-          return (
-            <Accordion
-              key={i}
-              contentVisible={false}
-              visibleElement={
-                <StyledImage
-                  source={IC_ARR_UP}
-                />
-              }
-              invisibleElement={
-                <StyledImage
-                  source={IC_ARR_DOWN}
-                />
-              }
-              header={
-                <View style={{ height: 28 }}>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      color: 'blue',
-                    }}
-                  >
-                    {param.title}
-                  </Text>
-                </View>
-              }
-            >
-              <Text style={{ fontSize: 20 }}>{param.body}</Text>
-            </Accordion>
-          );
-        })}
-      </ScrollView>
+      <Accordion />
     </Container>
   );
 };
